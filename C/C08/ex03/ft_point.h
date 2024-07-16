770cbf23a71d6418aaf28cce949d63af3fdb860a
Pere Rovira <122680815+provca@users.noreply.github.com>
2024-07-16 18:56:33 +0200

    Update ft_point.h
    Add comments

--- C/C08/ex03/ft_point.h
+++ C/C08/ex03/ft_point.h
@@ -9,7 +9,30 @@
 /*   Updated: 2024/07/16 17:22:54 by provira-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
+/*
+ * Header: FT_POINT_H
+ * -----------------
+ * Defines a structure `t_point` representing a point with integer coordinates.
+ *
+ * Structure:
+ * - typedef struct s_point: Defines a structure named `s_point`
+ *   with two members:
+ *   - int x: Represents the x-coordinate of the point.
+ *   - int y: Represents the y-coordinate of the point.
+ * - typedef t_point: Alias `t_point` for `struct s_point`.
+ *
+ * Usage:
+ * - This header allows programs to work with points in a Cartesian 
+ *   coordinate system.
+ * - Example usage:
+ *   ```
+ *   t_point pt1 = {10, 20};
+ *   pt1.x = 15;
+ *   printf("Coordinates: (%d, %d)\n", pt1.x, pt1.y);
+ *   ```
+ *   This snippet declares a point `pt1` with coordinates (10, 20), 
+ *   updates the x-coordinate, and prints the coordinates using printf.
+ */
 #ifndef FT_POINT_H
 # define FT_POINT_H
 
